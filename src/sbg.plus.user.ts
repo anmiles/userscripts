83eb6855e32b3cf617a91ac3cb95ece1744c848c
Anatoliy Oblaukhov <mail@anmiles.net>
2024-02-07 12:32:05 +0300

    sbg * bump new version

--- src/sbg.plus.user.ts
+++ src/sbg.plus.user.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           SBG plus
 // @namespace      sbg
-// @version        0.9.62
+// @version        0.9.63
 // @updateURL      https://anmiles.net/userscripts/sbg.plus.user.js
 // @downloadURL    https://anmiles.net/userscripts/sbg.plus.user.js
 // @description    Extended functionality for SBG
@@ -12,7 +12,7 @@
 // @grant          none
 // ==/UserScript==
 
-window.__sbg_plus_version = '0.9.62';
+window.__sbg_plus_version = '0.9.63';
 
 interface Window {
 	ol: Ol;
